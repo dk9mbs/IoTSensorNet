28b7f564a4e1aff51a771453531c8ec8547e96b3
dk9mbs <dk9mbs@gmail.com>
2023-07-28 09:13:38 +0200

    dew_point lib added

---
+++ restapi/plugins/iot_dew_point.py
@@ -0,0 +1,37 @@
+class Iot_DewPoint:
+    def __init__(self, celsius, humidity):
+        self._celsius=celsius
+        self._humidity=humidity
+        pass
+
+
+    def Calc ():
+        a=0
+        b=0
+
+        if self._celsius >= 0:
+            a = 7.5
+            b = 237.3
+        elif self._celsius < 0:
+            a = 7.6
+            b = 240.7
+
+        # Sättigungsdampfdruck (hPa)
+        sdd = 6.1078 * math.pow(10, (a * self._celsius) / (b + self._celsius))
+
+        # Dampfdruck (hPa)
+        dd = sdd * (self._humidity / 100)
+
+        # v-Parameter
+        v = math.log10(dd / 6.1078)
+
+        # Taupunkttemperatur (°C)
+        td = (b * v) / (a - v)
+
+        # Runden 1 Nachkommastelle
+        #td =  math.round(td * 10) / 10
+
+        return td
+
+#print(calcdewpoint(25.6, 47))
+#return msg;
