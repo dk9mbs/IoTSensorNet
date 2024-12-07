d092bcda1b3c0f4e7a6efb53500c162ca9878c65
dk9mbs <dk9mbs@gmail.com>
2024-12-07 14:13:28 +0100

    notify status

--- restapi/plugins/iot_plugin_shelly_notify_status.py
+++ restapi/plugins/iot_plugin_shelly_notify_status.py
@@ -9,27 +9,34 @@ from services.mqtt_client import MqttClient
 
 logger=log.create_logger(__name__)
 
-def __validate(params):
+def _validate(params):
     if 'data' not in params:
         return False
     if 'method' not in params['data']:
         return False
-    if 'id' not in params['data']:
+    if 'src' not in params['data']:
         return False
     if 'params' not in params['data']:
-        return False
-    if 'topic' not in params:
         return False
 
     return True
 
 def execute(context, plugin_context, params):
-    print (params)
-    if not __validate(params):
+    if not _validate(params):
         logger.warning(f"Missings params {params}")
         return
 
+    id=params['data']['src']
 
+    device=iot_device.objects(context).select().where(iot_device.id==id).to_entity()
+    if device==None:
+        device=iot_device()
+        device.id.value=id
+        device.name.value=id
+        device.address.value="0.0.0.0"
+        device.vendor_id.value="shelly"
+        device.insert(context)
+    
     #device=iot_device.objects(context).select().where(iot_device.id==shelly_external_id)
     #if device==None:
     #    logger.warning(f"Device not found in iot_device {shelly_external_id}")
