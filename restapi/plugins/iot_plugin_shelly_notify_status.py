0102efac14d0a8869fc043ae42d2f7b30f3c7db0
dk9mbs <dk9mbs@gmail.com>
2025-01-15 11:34:34 +0100

    notify plugin

--- restapi/plugins/iot_plugin_shelly_notify_status.py
+++ restapi/plugins/iot_plugin_shelly_notify_status.py
@@ -38,20 +38,36 @@ def execute(context, plugin_context, params):
     wlan=None
     version=None
     version_available=None
+    shelly_params={}
 
     if 'params' in params['data']:
         if 'wifi' in params['data']['params']:
             wlan=params['data']['params']['wifi']
+            shelly_params=params['data']['params']
 
     if 'result' in params['data']:
         if 'wifi' in params['data']['result']:
             wlan=params['data']['result']['wifi']
-        
+            shelly_params=params['data']['result']
+
         if 'ver' in params['data']['result']:
             version=params['data']['result']['ver']
 
         if id=='Shelly.CheckForUpdate' and 'stable' in params['data']['result']:
             version_available=params['data']['result']['stable']['version']
+
+    if shelly_params!={}:
+        for key, value in shelly_params.items():
+            if str(key).startswith("switch"):
+                switch=value
+
+                channel=iot_device_channel()
+                channel.device_id.value=src
+                channel.channel.value=str(key).split(":")[1]
+                channel.name.value="<new>"
+                channel.channel_value.value=switch['output']
+                channel.insert(context)
+                #print(f"{key} --> {value}")
 
     if version_available!=None:
         device.version_available.value=version_available
